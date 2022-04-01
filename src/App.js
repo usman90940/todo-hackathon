696968204544bb0e094084d36fc6bf28947251c2
usman90940 <usmansaeed90940@gmail.com>
2022-04-01 13:46:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+import _ from "lodash";
+import uuid from "react-uuid";
 
 function App() {
+  const [text, setText] = useState("")
+  const [state, setState] = useState({
+    "todo": {
+      title: "Todo",
+      items: []
+    },
+    "in-progress": {
+      title: "In Progress",
+      items: []
+    },
+    "done": {
+      title: "Completed",
+      items: []
+    }
+  })
+
+  const handleDragEnd = ({destination, source}) => {
+    console.log(destination, source)
+    if (!destination) {
+      return
+    }
+    // Creating a copy of item before removing it from state
+    const prevItem = {...state[source.droppableId].items[source.index]}
+
+    setState(prev => {
+      prev = {...prev}
+      // Remove from previous items array
+      prev[source.droppableId].items.splice(source.index, 1)
+      // Adding to new items array location
+      prev[destination.droppableId].items.splice(destination.index, 0, prevItem)
+
+      return prev
+    })
+  }
+  const deleteItem = (id) => {
+    console.log(state)
+    setState(state.source.droppableId.filter((x) => x.id !== id));
+  };
+  const addItem = () => {
+    setState(prev => {
+      return { ...prev, todo: {
+          title: "Todo",
+          items: [
+            {
+              id: uuid(),
+              name: text
+            },
+            ...prev.todo.items
+          ]
+        }
+      }
+    })
+
+    setText("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder = 'Add Todo'/>
+        <button onClick={addItem}>Add</button>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {_.map(state, (data, key) => {
+          return(
+            <div key={key} className={"column"}>
+              <h3>{data.title}</h3>
+              <Droppable droppableId={key}>
+                {(provided, snapshot) => {
+                  return(
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={"droppable-col"}
+                    >
+                      {data.items.map((el, index) => {
+                        return(
+                          <Draggable key={el.id} index={index} draggableId={el.id}>
+                            {(provided, snapshot) => {
+                              
+                              return(
+                                <div
+                                  className={`item ${snapshot.isDragging && "dragging"}`}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {el.name}
+                                <button onClick={() => deleteItem(el.id)}>Delete</button>
+
+                                </div>
+                              )
+                            }}
+                          </Draggable>
+                        )
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  )
+                }}
+              </Droppable>
+            </div>
+          )
+        })}
+      </DragDropContext>
     </div>
   );
 }
